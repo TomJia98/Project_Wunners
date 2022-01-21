97158586e8c8a2d102e35c141c0f632ec975d455
TomJia98 <Tomchristian1998@gmail.com>
2022-01-21 14:55:42 +0800

    got the words api call working

--- assets/javaScript/Toms_js_impliment.js
+++ assets/javaScript/Toms_js_impliment.js
@@ -1,1 +1,14 @@
-// check
+var word = "train"
+fetch("https://wordsapiv1.p.rapidapi.com/words/"+word+"/typeOf", {      
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+		"x-rapidapi-key": "89589060b5mshffab6adbe6bfb7ap1a6f99jsnddb2235d4e56"
+	}
+})
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+      console.log(data);
+  })
