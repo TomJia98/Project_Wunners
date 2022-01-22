06a6b86f625ce3168e5a6baede20fdf7cebfe769
TomJia98 <Tomchristian1998@gmail.com>
2022-01-22 17:38:20 +0800

    got the lyric changing working, going to try to get it to only do certain words

--- assets/javaScript/Toms_js_impliment.js
+++ assets/javaScript/Toms_js_impliment.js
@@ -1,5 +1,32 @@
-var word = "train"
-fetch("https://wordsapiv1.p.rapidapi.com/words/"+word+"/typeOf", {      
+
+var chosenSong = {
+    songname: "bazinga",
+    lyrics: "party rock is in the house tonight ikfgbsdv everybody is going to lose their minds"
+}
+
+function getWords(text){
+    let x = text.replace(/[^A-Za-z0-9]+/g, " ");
+    let newArr = x.trim().split(" ");
+    console.log(newArr);
+    return newArr;
+}
+var lyricsArr = getWords(chosenSong.lyrics);
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+};
+
+var loop = 0;
+
+ function changeLyrics(){
+    if (loop < lyricsArr.length){
+    
+    if (lyricsArr[loop].length > 4){
+
+
+    var word =  lyricsArr[loop];
+fetch("https://wordsapiv1.p.rapidapi.com/words/"+ word +"/typeOf", {      
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
@@ -10,5 +37,37 @@ fetch("https://wordsapiv1.p.rapidapi.com/words/"+word+"/typeOf", {
     return response.json();
   })
   .then(function (data) {
-      console.log(data);
+      if(data.typeOf.length ==0){
+        console.log(lyricsArr[loop] + " --- " + "no similar words, skipping");  
+        loop++;
+          changeLyrics();
+      }
+      else {
+      lyricsArr[loop] = data.typeOf[0]
+      console.log(lyricsArr);
+loop++
+changeLyrics()
+}
   })
+  .catch(function(){
+      console.log(lyricsArr[loop] + " --- " +"word not found, skipping");
+      loop++
+      changeLyrics()
+  })
+
+}
+else {
+    loop++;
+    changeLyrics();
+}}
+
+else {
+    console.log(lyricsArr, "<---")
+    var finished = lyricsArr.join(" ");
+    console.log(finished)
+    }
+
+}
+
+changeLyrics();
+
