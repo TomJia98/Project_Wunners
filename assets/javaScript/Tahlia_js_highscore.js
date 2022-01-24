fe2b4fc92b35a89a3a00cb4e95f3216ed40271d6
Tahlia La Galia <tahlia.lagalia@gmail.com>
2022-01-24 21:17:38 +0800

    Created highscore page

--- assets/javaScript/Tahlia_js_highscore.js
+++ assets/javaScript/Tahlia_js_highscore.js
@@ -1,1 +1,50 @@
-//Hello my name ia Tahlia.
+var timeTaken = 20;
+
+const amountofSongs = $("#amount-of-songs");
+const username = $("#username");
+const highScoresList = $("#scores");
+const highScores = JSON.parse(localStorage.getItem("highscore")) || [];
+
+const saveScoreBtn = $("#save");
+const finalScore = $("#final-score");
+
+const MAX_HIGH_SCORES = 5;
+
+function displayFinalTime() {
+  finalScore.text(timeTaken);
+}
+
+finalScore.innerText = highScores;
+
+username.on("keyup", () => {
+  saveScoreBtn.disabled = !username.value;
+});
+
+var score = {
+  name: username,
+  amountofSongs: amountofSongs,
+  finalScore: timeTaken,
+};
+
+var array = $.map(score, function (value, index) {
+  return [value];
+});
+
+saveScoreBtn.on("click", function saveHighScore(e) {
+  e.preventDefault();
+  score.name = username.val();
+  localStorage.setItem("score", JSON.stringify(score));
+});
+
+
+function settingUl() {
+  const li = $("<li>");
+  localStorage.setItem("highscore", JSON.stringify(score));
+  li.text(score.name + score.amountofSongs + score.finalScore);
+  li.addClass("high-score");
+  li.append(highScoresList);
+}
+
+displayFinalTime();
+settingUl();
+console.log(highScores);
