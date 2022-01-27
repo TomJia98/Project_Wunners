d72a7aa1744ee67b1649368ff25ab7be399ce6b7
Tahlia La Galia <tahlia.lagalia@gmail.com>
2022-01-27 20:39:06 +0800

    Finalised hs page

--- assets/javaScript/Tahlia_js_highscore.js
+++ assets/javaScript/Tahlia_js_highscore.js
@@ -1,46 +1,77 @@
 var timeTaken = 20; //using this for testing
 
 const amountofSongs = $("#amount-of-songs");
-const username = $("#username");
+const username = $(".username");
 const highScoresList = $("#scores");
-const highScores = JSON.parse(localStorage.getItem("highscore")) || [];
+let highScores = JSON.parse(localStorage.getItem("highscore")) || [];
 
 const saveScoreBtn = $("#save");
+const tryAgainBtn = $("#try-again");
 const finalScore = $("#final-score");
 
-function displayFinalTime() {
+function init() {
+  //Places the game time as the final score
   finalScore.text(timeTaken);
+
+  //Grabs the last high score
+  const latestScore = highScores[highScores.length - 1];
+  if (latestScore !== undefined) {
+    finalScore.innerText = latestScore.finalScore;
+  }
+
+  //Creates an li for every score that is stored within localstorage
+  for (var i = 0; i < highScores.length; i++) {
+    settingLi(highScores[i]);
+  }
 }
 
-finalScore.innerText = highScores;
-
-var score = {
-  name: "",
-  amountofSongs: amountofSongs,
-  finalScore: timeTaken,
-};
-
 saveScoreBtn.on("click", function saveHighScore(e) {
-  e.preventDefault();
-  score.name = username.val();
-  localStorage.setItem("score", JSON.stringify(score));
-  settingUl();
+  //When user clicks on save, create new score
+  var newScore = {
+    name: username.val(),
+    amountofSongsValue: amountofSongs.val(),
+    finalScore: timeTaken,
+  };
+
+  //Adds new score to highScores
+  highScores.push(newScore);
+
+  //Saves highScores to localstorage
+  localStorage.setItem("highscore", JSON.stringify(highScores));
+
+  //Hides the username input
+  username.attr("style", "display: none;");
+
+  //If hiscore > 5 clear local storage and ul
+  if (highScores.length > 4) {
+    localStorage.clear();
+    highScoresList.empty();
+    highScores = [];
+  }
+
+  //Adds li with new score
+  settingLi(newScore);
 });
 
 function settingUl() {
   const li = $("<li>");
-  localStorage.setItem("highscore", JSON.stringify(score));
-  li.text(score.name + score.amountofSongs + score.finalScore);
+  li.text(
+    "Name: " +
+      inputScore.name +
+      " " +
+      "Amount of songs: " +
+      inputScore.amountofSongsValue +
+      " " +
+      "Time taken: " +
+      inputScore.finalScore
+  );
   li.addClass("high-score");
-  li.append(highScoresList);
-}
-
-function displayScores(object) {
-  if (object) {
-    (settingUl).html(highScoresList);
-  }
+  highScoresList.append(li);
 }
 displayScores(highScores);
 
-displayFinalTime(); //add this to the on click for when the game is finished
-console.log(highScores);
+tryAgainBtn.on("click", function resetPage() {
+  location.reload();
+});
+
+init();
