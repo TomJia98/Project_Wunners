c18643c68a8134053af96f10e1c5b006ed4c4cd7
Tahlia La Galia <tahlia.lagalia@gmail.com>
2022-01-27 20:36:50 +0800

    Finalised hs page

--- assets/javaScript/Tahlia_js_highscore.js
+++ assets/javaScript/Tahlia_js_highscore.js
@@ -1,56 +1,60 @@
 var timeTaken = 20; //using this for testing
 
 const amountofSongs = $("#amount-of-songs");
-const username = $("#username");
+const username = $(".username");
 const highScoresList = $("#scores");
-const highScores = JSON.parse(localStorage.getItem("highscore")) || [];
+let highScores = JSON.parse(localStorage.getItem("highscore")) || [];
 
 const saveScoreBtn = $("#save");
+const tryAgainBtn = $("#try-again");
 const finalScore = $("#final-score");
 
 function init() {
+  //Places the game time as the final score
+  finalScore.text(timeTaken);
+
+  //Grabs the last high score
+  const latestScore = highScores[highScores.length - 1];
+  if (latestScore !== undefined) {
+    finalScore.innerText = latestScore.finalScore;
+  }
+
+  //Creates an li for every score that is stored within localstorage
   for (var i = 0; i < highScores.length; i++) {
-    //creates an li for every score that is stored within localstorage
     settingLi(highScores[i]);
   }
 }
 
-function displayFinalTime() {
-  finalScore.text(timeTaken);
-}
-
-finalScore.innerText = highScores;
-
 saveScoreBtn.on("click", function saveHighScore(e) {
-  // when user clicks on save
-  // create new score
-  // add new score to highScores
-  // save highScores to localstorage
-  // add li with new score
-
+  //When user clicks on save, create new score
   var newScore = {
     name: username.val(),
-    // amountofSongs: amountofSongs,
     amountofSongsValue: amountofSongs.val(),
     finalScore: timeTaken,
   };
 
+  //Adds new score to highScores
   highScores.push(newScore);
 
+  //Saves highScores to localstorage
   localStorage.setItem("highscore", JSON.stringify(highScores));
 
-  // if hiscore > 5 clear local storage and ul
-  if (highScores > 5) {
+  //Hides the username input
+  username.attr("style", "display: none;");
+
+  //If hiscore > 5 clear local storage and ul
+  if (highScores.length > 4) {
     localStorage.clear();
     highScoresList.empty();
+    highScores = [];
   }
 
+  //Adds li with new score
   settingLi(newScore);
 });
 
 function settingLi(inputScore) {
   const li = $("<li>");
-
   li.text(
     "Name: " +
       inputScore.name +
@@ -63,9 +67,10 @@ function settingLi(inputScore) {
   );
   li.addClass("high-score");
   highScoresList.append(li);
-
-  console.log(highScoresList);
 }
 
-displayFinalTime(); //add this to the on click for when the game is finished
+tryAgainBtn.on("click", function resetPage() {
+  location.reload();
+});
+
 init();
